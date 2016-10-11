a8277750cd432e17c7f9f60b9666aa3e63d5cb1d
Nacho Vidri <nacho@pompeiibrand.com>
2016-10-11 11:30:31 +0200

    exercise: each

---
+++ collectionmethods.rb
@@ -0,0 +1,86 @@
+class Home
+  attr_reader(:name, :city, :capacity, :price)
+
+  def initialize(name, city, capacity, price)
+    @name = name
+    @city = city
+    @capacity = capacity
+    @price = price
+  end
+end
+
+
+homes = [
+  Home.new("Nizar's place", "San Juan", 2, 42),
+  Home.new("Fernando's place", "Seville", 5, 47),
+  Home.new("Josh's place", "Pittsburgh", 3, 41),
+  Home.new("Gonzalo's place", "Málaga", 2, 45),
+  Home.new("Ariel's place", "San Juan", 4, 49)
+]
+
+
+homes.each do |hm|
+  puts "#{hm.name} in #{hm.city} is ¢#{hm.price} per night"
+end
+
+# prices = homes.map do |hm|
+# 	hm.price
+# end
+
+# puts prices
+=begin
+sum_prices = 0
+average_price = 0
+
+prices.each do |pr|
+	sum_prices = sum_prices + pr 
+	average_price = sum_prices / prices.length
+end
+
+puts sum_prices
+
+puts average_price
+
+
+puts prices.reduce(0) {|sum_prices, pr| sum_prices + pr}
+
+puts average_price = sum_prices / prices.length
+
+=end
+
+#EJERCICIO FINAL
+=begin
+homes = [
+  Home.new("Nizar's place", "San Juan", 2, 42),
+  Home.new("Fernando's place", "Seville", 5, 47),
+  Home.new("Josh's place", "Pittsburgh", 3, 41),
+  Home.new("Gonzalo's place", "Málaga", 2, 45),
+  Home.new("Ariel's place", "San Juan", 4, 49),
+  Home.new("Pepe's place", "Madrid", 9, 60),
+  Home.new("Nacho's place", "Barcelona", 4, 50),
+  Home.new("Juan's place", "London", 2, 43),
+  Home.new("Lolo's place", "Berlin", 5, 41),
+  Home.new("Ray's place", "San Juan", 6, 50)
+]
+
+# homes.each do |hm|
+# 	puts "#{hm.name} in #{hm.city} is ¢#{hm.price} per night and has capacity for #{hm.capacity}"
+# end
+
+
+# homes.sort
+
+# prices = homes.map {|hm| hm.price}
+
+# prices.sort
+
+# puts prices
+
+favourite_home = gets.chomp
+
+
+puts favourite_home
+=end
+
+
+
